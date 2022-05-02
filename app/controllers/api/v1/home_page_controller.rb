2bd23f89bd5d7f18ca708fba0d8f505688893542
Luiz Affonso <luizbuchhalter@outlook.com>
2022-05-02 00:57:55 -0300

    Add index to controller

--- app/controllers/api/v1/home_page_controller.rb
+++ app/controllers/api/v1/home_page_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class HomePageController < Api::V1::ApiController
       def index
+        @user = User.find(params[:id])
+        @posts = Post.all
       end
     end
   end
