900b21892c3cffbd66f257e8baf82dcc67cc5416
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 21:07:58 -0300

    Update use case with execute

--- app/src/messaging/application/use_cases/delete_post.rb
+++ app/src/messaging/application/use_cases/delete_post.rb
@@ -8,10 +8,18 @@ module Messaging
             publisher: EventManager::Publisher.instance
           )
         end
-        
+
         def initialize(all_posts:, publisher:)
           @all_posts = all_posts
           @publisher = publisher
+        end
+
+        def execute(post_id:, author:)
+          post = @all_posts.find_by_id(id: post_id)
+          @all_posts.destroy(id: post_id)
+          publish_post_deleted_event(post: post, author: author)
+        rescue Exceptions::PostDeleteException => e
+          raise_post_delete_exception(post: post, exception: e)
         end
       end
     end
