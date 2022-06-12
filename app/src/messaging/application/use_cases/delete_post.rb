76e7114960bcf067f902c9252a3854769f349bb3
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 21:07:22 -0300

    Add structure for delete use case

---
+++ app/src/messaging/application/use_cases/delete_post.rb
@@ -0,0 +1,14 @@
+module Messaging
+  module Application
+    module UseCases
+      class DeletePost
+        def self.build
+          new(
+            all_posts: Infra::Repositories::AllPosts.build,
+            publisher: EventManager::Publisher.instance
+          )
+        end
+      end
+    end
+  end
+end
