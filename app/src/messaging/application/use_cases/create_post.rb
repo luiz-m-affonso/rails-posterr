89886c91a2465a722a84805314a2c2d002cdacb6
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 21:03:27 -0300

    Add self build with repository, factory and publisher

--- app/src/messaging/application/use_cases/create_post.rb
+++ app/src/messaging/application/use_cases/create_post.rb
@@ -2,6 +2,13 @@ module Messaging
   module Application
     module UseCases
       class CreatePost
+        def self.build
+          new(
+            all_posts: Infra::Repositories::AllPosts.build,
+            post_factory: Infra::Factories::PostFactory.build,
+            publisher: EventManager::Publisher.instance
+          )
+        end
       end
     end
   end
