2c8dd2621312394a42fe3eaf03bde477ad3a395f
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 21:10:44 -0300

    Add to_hash method to post

--- app/src/messaging/domain/post.rb
+++ app/src/messaging/domain/post.rb
@@ -11,6 +11,17 @@ module Messaging
         @type = type
         @user_id = user_id
       end
+
+      def to_hash
+        {
+          id: @id,
+          title: @title,
+          description: @description,
+          text: @text,
+          type: @type,
+          user_id: @user_id
+        }
+      end
     end
   end
 end
