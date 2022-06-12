dfb0ca48f7aa2b54ee4b3bbfaf66737a63996f22
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 21:09:31 -0300

    Add execute to update post use case

--- app/src/messaging/application/use_cases/update_post.rb
+++ app/src/messaging/application/use_cases/update_post.rb
@@ -13,6 +13,20 @@ module Messaging
           @all_posts = all_posts
           @publisher = publisher
         end
+
+        def execute(post_id:, title:, description:, author:)
+          begin
+            post = @all_posts.find_by_id(id: post_id)
+            post.title = title
+            post.description = description
+            post.text = text
+            post.type = type
+            @all_posts.save!(post: post)
+            publish_post_updated_event(post: post, author: author)
+          rescue Excpetions::UpdatePostException => e
+            raise_update_post_exception(post: post, message: e.message)
+          end
+        end
       end
     end
   end
