fca79f4d502f994a7815e6459394d17a95d8131f
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-11 22:51:24 -0300

    fix: Remove extra tests

--- spec/test/messaging/domain/post_spec.rb
+++ spec/test/messaging/domain/post_spec.rb
@@ -22,31 +22,6 @@ module Messaging
           expect(post).to respond_to(:user_id)
         end
       end
-
-      describe '#to_hash' do
-        let(:post) {
-          described_class.new(
-            id: 1,
-            title: 'title',
-            description: 'description',
-            text: 'text',
-            type: 'type',
-            user_id: 1
-          )
-        }
-
-        it 'returns a hash' do
-          expect(post.to_hash).to be_a(Hash)
-        end
-
-        it 'returns a hash with the correct keys' do
-          expect(post.to_hash.keys).to eq([:id, :title, :description, :text, :type, :user_id])
-        end
-
-        it 'returns a hash with the correct values' do
-          expect(post.to_hash.values).to eq([1, 'title', 'description', 'text', 'type', 1])
-        end
-      end
     end
   end
 end
